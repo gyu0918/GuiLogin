de6b85f2c95b7dd36dfe245e56201349bf6bac98
junggkim <kjk1526@naver.com>
2025-03-12 17:56:00 +0900

    guiPractice

---
+++ final/JframePractice/src/Window.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    public Window() {
+        setTitle("Window");
+        setBounds(100, 100, 450, 300);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
